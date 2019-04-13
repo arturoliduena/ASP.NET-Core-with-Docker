aa0a9e04c54f943acf03ed6ca8f8d9b9bb956a6e
Arturo Lidueña <arturo.liduena@gmail.com>
2019-04-13 21:20:22 +0200

    add simple example

---
+++ console_app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 10;
+            var stop = 0;
+            while(counter >= stop)
+            {
+                Console.WriteLine($"Counter: {counter}");
+                counter--;
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
